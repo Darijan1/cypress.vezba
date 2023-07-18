6b7da302c1f5cd25e6c21b54b0cba141eabe44e8
LegendGodSrb <134584690+LegendGodSrb@users.noreply.github.com>
2023-07-18 12:46:44 +0200

    novo

---
+++ cypress/e2e/test/Sasvimnovo.cy.js
@@ -0,0 +1,7 @@
+describe("Buying new hoodi",()=>{  
+    it("Visiting Swag Labs",()=>{
+      cy.visit("https://www.saucedemo.com")
+      cy.clearAllCookies()
+      cy.clearAllLocalStorage()   
+      })
+    })
